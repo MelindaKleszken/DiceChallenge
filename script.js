f6d75815f2d8bd5b085f6b3e59b91e27e46c6462
MelindaKleszken <melindakleszken1021@gmail.com>
2020-12-17 16:18:07 +0000

    logic added to js

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 const button = document.getElementById("roll");
-const points = document.getElementById("points").innerHTML;
+const points = document.getElementById("points");
 const image = document.getElementById("dice");
+const player = document.getElementById("player-name");
 console.log(image.style);
-console.log(points);
+
 
 button.addEventListener("click", ()=>{
-    if (image.style.display == "none") {
+    if (button.innerHTML == "Play" || button.innerHTML == "Play Again?" ) {
         image.style.display = "block";
-        button.textContent = "roll again";
-    } else {
-        image.style.display = "none";
-        button.textContent = "roll"; 
+        button.innerHTML = "Roll";
+        player.innerHTML = "Player 1";
+        points.innerHTML = 0;
     }
- })
+    let x = Math.floor(Math.random() * 6) + 1;
+    let y = parseInt(points.innerHTML);
+    image.src = `./img/dice${x}.png`;
+    
+    if (x === 1) {
+        player.innerHTML = "You Lost!";
+        button.innerHTML = "Play Again?";
+    } else if (points.innerHTML < 20) {
+        y += x;
+        points.innerHTML = y
+    }
+    if (y >= 20) {
+        player.innerHTML = "Winner!";
+        button.innerHTML = "Play Again?";
+    }
+})
