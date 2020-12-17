5844fdb79d652edc2e675d75ecaf1796ec8b3b8c
MelindaKleszken <melindakleszken1021@gmail.com>
2020-12-17 14:00:41 +0000

    middle commit

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const button = document.getElementById("roll");
+const points = document.getElementById("points").innerHTML;
+const image = document.getElementById("dice");
+console.log(image.style);
+console.log(points);
 
+button.addEventListener("click", ()=>{
+    if (image.style.display == "none") {
+        image.style.display = "block";
+        button.textContent = "roll again";
+    } else {
+        image.style.display = "none";
+        button.textContent = "roll"; 
+    }
+ })
